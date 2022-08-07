6969f8c8c353bc3490d34cd6ac55aa2148077ea3
spriteboysz <sprite_bx@126.com>
2022-08-07 21:01:34 +0800

    2022080721

---
+++ LeetcodeCC/algorithm/P0000_HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    void print(string s) {
+        cout << s << endl;
+    }
+};
+
+
+int main0000() {
+    Solution().print("Hello World!");
+    return 0;
+}
