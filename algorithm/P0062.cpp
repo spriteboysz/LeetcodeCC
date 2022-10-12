7c9df60d576bdeb7f180541430d9ef148aa88ccf
spriteboysz <sprite_bx@126.com>
2022-10-12 22:37:16 +0800

    2022101222

---
+++ algorithm/P0062.cpp
@@ -0,0 +1,33 @@
+/**
+ * Author: Deean
+ * Date: 2022-10-12 22:34
+ * FileName: algorithm/P0062.cpp
+ * Description: 
+ */
+
+#include <leetcode.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> grid(m, vector<int>(n));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || j == 0) {
+                    grid[i][j] = 1;
+                } else {
+                    grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+                }
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+};
+
+int main() {
+    auto ans = Solution().uniquePaths(3, 7);
+    cout << toString(ans) << endl;
+    return 0;
+}
---
+++ offer/OII0098.cpp
@@ -0,0 +1,33 @@
+/**
+ * Author: Deean
+ * Date: 2022-10-12 22:30
+ * FileName: offer/OII0098.cpp
+ * Description: 剑指 Offer II 098. 路径的数目
+ */
+
+#include <leetcode.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> grid(m, vector<int>(n));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || j == 0) {
+                    grid[i][j] = 1;
+                } else {
+                    grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+                }
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+};
+
+int main() {
+    auto ans = Solution().uniquePaths(3, 7);
+    cout << toString(ans) << endl;
+    return 0;
+}
