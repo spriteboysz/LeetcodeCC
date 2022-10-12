e77bec18c5778f526d1567b64be426b40e7fe230
spriteboysz <sprite_bx@126.com>
2022-10-12 21:15:02 +0800

    2022101221

---
+++ algorithm/P1302.cpp
@@ -0,0 +1,42 @@
+/**
+ * Author: Deean
+ * Date: 2022-10-11 23:44
+ * FileName: algorithm/P1302.cpp
+ * Description: 1302. 层数最深叶子节点的和
+ */
+
+#include <leetcode.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode *root) {
+        int leaves = 0;
+        queue<TreeNode *> queue;
+        queue.emplace(root);
+        while (!queue.empty()) {
+            leaves = 0;
+            int size = queue.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode *node = queue.front();
+                queue.pop();
+                leaves += node->val;
+                if (node->left != nullptr) {
+                    queue.emplace(node->left);
+                }
+                if (node->right != nullptr) {
+                    queue.emplace(node->right);
+                }
+            }
+        }
+        return leaves;
+    }
+};
+
+int main() {
+    TreeNode *root = stringToTree("[6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]");
+    auto ans = Solution().deepestLeavesSum(root);
+    cout << toString(ans) << endl;
+    return 0;
+}
---
+++ offer/OII0055.cpp
@@ -0,0 +1,51 @@
+/**
+ * Author: Deean
+ * Date: 2022-10-12 00:02
+ * FileName: offer/OII0055.cpp
+ * Description: 剑指 Offer II 055. 二叉搜索树迭代器
+ */
+
+#include <leetcode.h>
+
+using namespace std;
+
+class BSTIterator {
+private:
+    int index;
+    vector<int> values;
+public:
+    void dfs(TreeNode *root) {
+        if (root == nullptr) return;
+        dfs(root->left);
+        values.push_back(root->val);
+        dfs(root->right);
+    }
+
+    BSTIterator(TreeNode *root) {
+        index = 0;
+        dfs(root);
+    }
+
+    int next() {
+        return values[index++];
+    }
+
+    bool hasNext() {
+        return index < values.size();
+    }
+};
+
+int main() {
+    TreeNode *root = stringToTree("[7,3,15,null,null,9,20]");
+    BSTIterator *bSTIterator = new BSTIterator(root);
+    cout << bSTIterator->next() << endl;    // 返回 3
+    cout << bSTIterator->next() << endl;    // 返回 7
+    cout << bSTIterator->hasNext() << endl; // 返回 True
+    cout << bSTIterator->next() << endl;    // 返回 9
+    cout << bSTIterator->hasNext() << endl; // 返回 True
+    cout << bSTIterator->next() << endl;    // 返回 15
+    cout << bSTIterator->hasNext() << endl; // 返回 True
+    cout << bSTIterator->next() << endl;    // 返回 20
+    cout << bSTIterator->hasNext() << endl; // 返回 False
+    return 0;
+}
