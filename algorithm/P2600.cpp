be7cf657dfb263e967e4c6af3172d9ed7d6ccb1c
spriteboysz <sprite_bx@126.com>
2023-04-09 13:13:02 +0800

    2023040913

--- algorithm/P2600.cpp
+++ algorithm/P2600.cpp
@@ -12,6 +12,7 @@ using namespace std;
 class Solution {
 public:
     int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        if (numNegOnes < 0) return -1;
         if (k <= numOnes) {
             return k;
         }
